072ee83ff18d57c794143fda942473d7be39a349
CorinnaGeiselhart <corinna@bayern-mail.de>
2020-04-07 17:39:57 +0200

    fertig

--- 02-Calculator/index.js
+++ 02-Calculator/index.js
@@ -6,7 +6,7 @@ let numpad = document.querySelector(".numpad"),
     calculation = document.querySelector("#calculation"),
     solution = document.querySelector("#solution");
 
-//console.log(numpad);
+
 //console.log(numpad.children);
 window.calc = new Calculator(numpad, calculation, solution);
 
