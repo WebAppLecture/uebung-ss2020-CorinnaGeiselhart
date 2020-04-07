c9ca2640df1c06857149434e07ebfc1a7e0ffaf1
CorinnaGeiselhart <corinna@bayern-mail.de>
2020-04-07 14:51:19 +0200

    Zwischenstand 1

--- 02-Calculator/Calculator.js
+++ 02-Calculator/Calculator.js
@@ -3,14 +3,20 @@ import {MyMath} from "../01-MyMath/MyMath.js";
 export class Calculator {
 
     constructor(numpad, outputCalculation, outputSolution) {
+        this.numPad = numpad;
+        this.outputCalculation = outputCalculation;
+        this.outputSolution = outputSolution;
+        this.setupNumPad();
     }
 
     setupNumPad() { 
-
+        this.numPad.children.array.forEach(element => {
+            element.addEventListener("click", this.onButtonclick.bind(this));
+        });
     }
 
     onButtonClick(symbol) {
-    
+        console.log(symbol);
     }
 
     print(string) {
