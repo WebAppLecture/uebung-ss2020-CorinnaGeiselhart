24dfa95519f6db9db95888400bc20b51aca09be3
CorinnaGeiselhart <corinna@bayern-mail.de>
2020-04-07 11:46:09 +0200

    Bearbeitung - Aufgabe 1-3,5

--- 01-MyMath/MyMath.js
+++ 01-MyMath/MyMath.js
@@ -16,29 +16,46 @@ export class MyMath {
         // Hier referenziert es die Instanz der Klasse MyMath die wir gerade erstellen.
         // mit 'value * 1' erzwingen wir, dass value als number gelesen wird.
         this.value = value * 1; 
+        if(value === undefined){
+            this.value = 0;
+        }
     }
 
     add(value) {
-
+        this.value += value;
+        return this;
     }
 
     subtract(value) {
-
+        this.value -= value;
+        return this;
     }
 
     multiply(value) {
-
+        this.value *= value;
+        return this;
     }
 
     divide(value) {
-
+        if(value !== 0){
+            this.value /= value;
+        }
+        return this;
     }
 
     pow(value) {
-
+        if(value >= 0){
+            let a = this.value;
+            for(let i = 0; i < value-1; i++){
+                this.value*=a;
+            }
+            return this;
+        } else {
+            return "Kein valide Potenz. Nur postive Zahlen werden akzeptiert";
+        }
     }
 
     faculty() {
-
+        return this;
     }
 }
